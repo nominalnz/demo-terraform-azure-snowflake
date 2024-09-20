a7379e7efcf8db45e7dc3aa7a9504f390e156173
Nic Ninow <nic@nominal.nz>
2024-09-20 18:36:10 +1200

    Grant useradmin role

--- terraform/management/snowflake.tf
+++ terraform/management/snowflake.tf
@@ -1,3 +1,21 @@
 resource "snowflake_user" "useradmin" {
-  name = "USERADMIN"
+  name = "USERADMIN_USER"
+}
+
+# data "snowflake_roles" "useradmin" {
+#   like = "USERADMIN"
+# }
+
+# output "snowflake_roles_useradmin_output" {
+#   value = data.snowflake_roles.useradmin.roles
+# }
+
+# resource "snowflake_grant_account_role" "useradmin" {
+#   role_name = data.snowflake_roles.useradmin.roles[0].show_output[0].name
+#   user_name = snowflake_user.useradmin.name
+# }
+
+resource "snowflake_grant_account_role" "useradmin" {
+  role_name = "USERADMIN"
+  user_name = snowflake_user.useradmin.name
 }
